dbd6dc49ad6a0b651336c182742157e3046f92f4
Oscar Shoudt <shoudto@gmail.com>
2019-05-10 22:00:51 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class BankAccount
   
-  attr_accessor :balance 
-  attr_reader :name, :status  
+  attr_accessor :balance, :status 
+  attr_reader :name  
 
   def initialize(name) 
     @name = name
