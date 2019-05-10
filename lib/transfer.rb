e41b1b59511d08e5820ef8890c3ed5fecea520bc
Oscar Shoudt <shoudto@gmail.com>
2019-05-10 23:36:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,10 +19,13 @@ class Transfer
   
   def execute_transaction
     # binding.pry
-     
-    @sender.balance -= amount  
-    @receiver.balance += amount 
-    self.status = "complete"
+    if @status == "pending" 
+      @sender.balance -= amount  
+      @receiver.balance += amount 
+      self.status = "complete"
+    else 
+      
+    end 
     
   end 
 end
