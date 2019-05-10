0a67abcbc8da13e7835012468898d2522c42ba62
Oscar Shoudt <shoudto@gmail.com>
2019-05-10 23:39:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       @receiver.balance += amount 
       self.status = "complete"
      else 
-      @status = "rejected"
+      self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end 
   end 
