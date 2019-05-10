f1d4ff2bdf411680bc8616a786f5108cc11844e7
Oscar Shoudt <shoudto@gmail.com>
2019-05-10 22:06:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,11 @@
 class Transfer
   
+  attr_accessor :status
   attr_reader :sender, :receiver  
   
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver 
+    @status = "pending"
   end 
 end
